d7edb1a284c83e8b2cf5f39965cbc6b796c48ca4
kbastani <kb@socialmoon.com>
2021-06-29 14:08:30 -0400

    Added CDC ingestion spec for Pinot in bootstrap-light.sh

---
+++ bootstrap-light.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Increase the max_connections to 10,000 for the MySQL database instance
+docker-compose exec mysql bash -c 'mysql -u root -p$MYSQL_ROOT_PASSWORD orderweb -e "SET GLOBAL max_connections = 10000;"'
+
+curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" \
+    http://localhost:8083/connectors/orderweb/config -d @debezium-mysql-connector-outbox.json
+
+# Copy the Pinot table and schema configurations to Pinot container and execute the add table command
+docker cp pinot/order-delivery-table-definition.json order-delivery-microservice-example_pinot_1:/opt/pinot
+docker cp pinot/order-delivery-schema-definition-cdc.json order-delivery-microservice-example_pinot_1:/opt/pinot
+docker exec order-delivery-microservice-example_pinot_1 bash -c "/opt/pinot/bin/pinot-admin.sh AddTable -tableConfigFile /opt/pinot/order-delivery-table-definition.json -schemaFile /opt/pinot/order-delivery-schema-definition-cdc.json -exec"
+
+# Open the browser for Pinot
+open http://localhost:9000
